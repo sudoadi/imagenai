8252fce5f0e830ad25c93335cd06c47c17289ab6
sudoadi <96442800+sudoadi@users.noreply.github.com>
2025-03-16 23:40:36 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-export default {
+var src_default = {
   async fetch(request, env) {
-    const inputs = {
-      prompt: "cyberpunk cat",
-    };
+    const url = new URL(request.url);
+    const prompt = url.searchParams.get("prompt") || "rdr2 arthur morgan seeing sunset"; // Default prompt if none is provided
 
+    const inputs = { prompt };
     const response = await env.AI.run(
       "@cf/stabilityai/stable-diffusion-xl-base-1.0",
-      inputs,
+      inputs
     );
 
     return new Response(response, {
-      headers: {
-        "content-type": "image/png",
-      },
+      headers: { "content-type": "image/png" }
     });
-  },
-} satisfies ExportedHandler<Env>;
+  }
+};
+
+export { src_default as default };
